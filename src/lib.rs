2facd43796dab479fa219dfa81eeb0a5e2e37269
Spencer Pope <spenceropope@gmail.com>
2023-07-25 20:09:41 -0400

    Add couple comments for future demo

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use wasm_bindgen::prelude::*;
 
+// extern tells Rust that we want to call some externally defined functions. The attribute says "wasm-bindgen knows how to find these functions".
 #[wasm_bindgen]
 extern "C" {
     pub fn alert(s: &str);
 }
 
+// function for javascript to call
+// (attribute is modifying this fn too)
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {name}!"));
